5cbc4cec2e2511a6009849c53bdcda90a6f907eb
euonymus <euonymus0220@gmail.com>
2018-06-15 19:12:41 +0900

    api

--- src/Controller/GluonsController.php
+++ src/Controller/GluonsController.php
@@ -1,5 +1,6 @@
 <?php
 namespace App\Controller;
+use Cake\Event\Event;
 
 use App\Controller\AppController;
 
@@ -13,7 +14,7 @@ use App\Utils\U;
 /**
  * Gluons Controller
  *
- * @property \App\Model\Table\SubjectsTable $Subjects
+ * @property \App\Model\Table\SubjectsTable $Relations
  */
 class GluonsController extends AppController
 {
@@ -33,6 +34,16 @@ class GluonsController extends AppController
         return parent::isAuthorized($user);
     }
 
+    public function beforeFilter(Event $event)
+    {
+        $this->Auth->allow(['view', 'byQuarkProperty']);
+
+	$this->paginate = [
+	   'contain' => ['Actives', 'Passives'],
+           'limit' => 100
+        ];
+    }
+
     public function initialize()
     {
       parent::initialize();
@@ -42,19 +53,30 @@ class GluonsController extends AppController
       $this->response->header("Access-Control-Allow-Origin: *");
     }
 
-    public function view($quark_id = null, $quark_property_id = 'active')
+    // $quark_property_id is quark_property_id, but 'active', 'passive' are exceptionally accepted
+    public function byQuarkProperty($quark_id = null, $quark_property_id = 'active')
     {
-      $this->paginate = [
-	 'contain' => ['Actives', 'Passives'],
-	 'limit' => 100
-      ];
-
       $Relations = TableRegistry::get('Relations');
 
       if (in_array($quark_property_id, ['active', 'passive'])) {
 	$where = $Relations->whereByNoQuarkProperty($quark_id, $quark_property_id);
       } else {
 	$where = $Relations->whereByQuarkProperty($quark_id, $quark_property_id);
+      }
+      $query = $Relations->find()->where($where)->order(['Relations.start' => 'Desc']);
+      $this->set('articles', $this->paginate($query));
+      $this->set('_serialize', 'articles');
+    }
+
+    public function view($quark_id = null, $gluon_sides = 'active')
+    {
+      $Relations = TableRegistry::get('Relations');
+      if ($gluon_sides == 'active') {
+	$where = ['Relations.active_id' => $quark_id];
+      } elseif ($gluon_sides == 'passive') {
+	$where = ['Relations.passive_id' => $quark_id];
+      } else {
+	$where = $Relations->whereNoRecord();
       }
       $query = $Relations->find()->where($where)->order(['Relations.start' => 'Desc']);
       $this->set('articles', $this->paginate($query));
