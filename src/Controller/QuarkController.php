9d80e028c8a3ab516f079183fb912e2e6c6dc804
euonymus <euonymus0220@gmail.com>
2018-06-16 15:46:18 +0900

    chore

--- src/Controller/QuarkController.php
+++ src/Controller/QuarkController.php
@@ -47,54 +47,9 @@ class QuarkController extends AppController
       $Subjects = TableRegistry::get('Subjects');
       
       $query = $Subjects->find()->where($Subjects->wherePrivacyName($name));
-      /* $this->set('articles', $this->paginate($query)); */
       $this->set('articles', $query->first());
-   
-      /* $this->paginate = [ */
-      /* 			 'table' => '', */
-      /* 			 'conditions' => ['user_id' => $this->Auth->user('id')], */
-      /* 			 ]; */
-      /* $this->set('articles', $this->paginate()); */
-      // JsonView がシリアライズするべきビュー変数を指定する
       $this->set('_serialize', 'articles');
 	
-      /*
-	try {
-	  \App\Model\Table\SubjectsTable::$cachedRead = true;
-	  $subject = $this->Subjects->getRelationsByName($name, $contain, 2, $second_type);
-	} catch(\Exception $e) {
-	  try {
-	    $forRedirect = $this->Subjects->get($name);
-	  } catch(\Exception $e) {
-	    throw new NotFoundException('Record not found in table "subjects"');
-	  }
-	  $suffix = ($second_type == 'active') ? '' : '/' . $second_type;
-	  return $this->redirect('/subjects/relations/' . urlencode($forRedirect->name) . $suffix, 301);
-	}
-
-	// just in case;
-	if (!$subject) return $this->redirect('/');
-
-	$title_second_level = '';
-	if ($second_type == 'passive') {
-	  //$title_second_level = '[' . $second_type . ' relation]';
-	  $title_second_level = '[' . $second_type . ']';
-	} elseif ($second_type == 'none') {
-	  //$title_second_level = '[no second relation]';
-	  $title_second_level = '[simple]';
-	}
-	$title = $subject->name . $title_second_level;
-
-	// build canonical
-	$second_type_path = ($second_type == 'active') ? '' : '/' . $second_type;
-	$domain = Router::url('/', true);
-	$canonical = $domain . 'subjects/relations/' . $name . $second_type_path;
-
-        $QpropertyGtypes = TableRegistry::get('QpropertyGtypes');
-	$this->set('qproperty_gtypes', $QpropertyGtypes->find());
-        $this->set(compact('subject', 'second_type', 'title', 'canonical'));
-        $this->set('_serialize', ['subject']);
-      */
     }
 
 }
