a6cec46da6cbb2d96d7bab4387394bf7f495e1f9
euonymus <euonymus0220@gmail.com>
2018-09-23 01:00:07 +0900

    pickup

--- src/Controller/QuarksController.php
+++ src/Controller/QuarksController.php
@@ -309,15 +309,16 @@ class QuarksController extends AppController
 		     ];
 
       $ja_pickup_ids = [
+		     '0cf51d72-b415-496a-b94e-92e6f07e629e' => 'active', // 佐藤健 (俳優)
 		     'b9ac937a-bce7-417e-9cda-617ecf104878' => 'active', // 樹木希林
 		     '8cdbd05a-0fa1-4b2f-8ea5-baba549c3a36' => 'active', // アントニオ猪木
 		     '6bd9de91-815e-4d5d-aafb-7ce3a43c62c1' => 'active', // 宮田聡子
 		     '5a7fb826-8d86-4f7a-a74c-90d9296d82ba' => 'active', // 高須克弥
 		     '0726645c-0cfd-4b88-8e12-7ef8393de1c6' => 'active', // 宮川紗江
-		     '00da8e56-e670-4b98-86f4-fe0720df66e8' => 'active', // 石破茂
 		     '71543dad-37b0-4b97-9dec-969033d71a91' => 'active', // さくらももこ
 		     'a3a93852-cf59-4de3-a144-3cf2b607a332' => 'active', // 湊伸治
 
+		     //'00da8e56-e670-4b98-86f4-fe0720df66e8' => 'active', // 石破茂
 		     //'021e29e3-b3c9-4fee-bbe4-26a6e44efb40' => 'passive', // Hana倶楽部投資金未返還問題
 		     //'6257e52a-f888-4d4c-a4c2-18e4cccfa94c' => 'active',  // 共謀罪
 		     //'df03fae8-16b0-4ba8-8309-d5ca64c1eb28' => 'active',  // NEM不正送金事件
