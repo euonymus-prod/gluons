accaa519b48ecd577b3441cb3178a29d1faa5c2a
euonymus <euonymus0220@gmail.com>
2019-08-14 17:14:26 +0900

    optimization

--- src/Controller/GraphController.php
+++ src/Controller/GraphController.php
@@ -10,7 +10,6 @@ use Cake\Cache\Cache;
 use Cake\Routing\Router;
 use App\Utils\U;
 
-use GraphAware\Neo4j\Client\ClientBuilder;
 use Cake\Log\Log;
 
 /**
@@ -40,7 +39,8 @@ class GraphController extends AppController
     // API endpoint:  /graph/:name
     public function name($name = null)
     {
-        $graph = $this->_getOnesGraph($name);
+        $Neo4j = TableRegistry::get('Neo4j');
+        $graph = $Neo4j::getOnesGraph($name);
         if (!$graph || count($graph) == 0) {
             $res = ['status' => 0, 'message' => 'Not found'];
         } else {
@@ -58,7 +58,8 @@ class GraphController extends AppController
             throw new NotFoundException(__('記事が見つかりません'));
         }
 
-        $graph = $this->_getOnesGraph($name, $privacy, $this->Auth->user('id'));
+        $Neo4j = TableRegistry::get('Neo4j');
+        $graph = $Neo4j::getOnesGraph($name, $privacy, $this->Auth->user('id'));
         if (!$graph || count($graph) == 0) {
             $res = ['status' => 0, 'message' => 'Not found'];
         } else {
@@ -69,107 +70,4 @@ class GraphController extends AppController
         $this->set('articles', $res);
         $this->set('_serialize', 'articles');
     }
-
-    public function _getOnesGraph($name, $privacy_mode = \App\Controller\AppController::PRIVACY_PUBLIC, $user_id = null)
-    {
-        if (($privacy_mode != \App\Controller\AppController::PRIVACY_PUBLIC) && is_null($user_id)) return false;
-
-        // build cypher query
-        $where = self::_wherePrivacy($privacy_mode, $user_id);
-        $query = 'MATCH (subject {name: {name}})-[relation]-(object) '
-               .$where
-               .'RETURN DISTINCT subject, object, relation';
-        $parameter = ['name' => $name];
-
-        // connect to neo4j
-        $client = ClientBuilder::create()
-                ->addConnection('http', 'http://neo4j:neo4jn30Aj@localhost:7474')
-                ->build();
-
-        // run cypher
-        $result = $client->run($query, $parameter);
-        if (!$result->records()) return false;
-
-        // format result array
-        $subject = $result->getRecord()->value('subject');
-        $ret = ['subject' => $this->_buildNodeArr($subject), 'relations' => []];
-
-        foreach ($result->getRecords() as $key => $record) {
-            $active = $this->_getActiveNode($record);
-            $passive = $this->_getPassiveNode($record);
-            $relation = $record->value('relation');
-
-            $ret['relations'][] = [
-                'relation' => $this->_buildRelationshipArr($relation),
-                'active' => $this->_buildNodeArr($active),
-                'passive' => $this->_buildNodeArr($passive),
-            ];
-        }
-        // Log::write('debug', $ret);
-        return $ret;
-    }
-
-    public static function _wherePrivacy($privacy_mode, $user_id = 1)
-    {
-        if ($privacy_mode == \App\Controller\AppController::PRIVACY_PUBLIC) {
-            // Only Public
-            return 'WHERE subject.is_private = false AND object.is_private = false ';
-        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_PRIVATE) {
-            // Only Private
-            return 'WHERE subject.is_private = true AND subject.user_id = '.$user_id.
-                   ' AND object.is_private = true AND object.user_id = '.$user_id. ' ';
-        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_ALL) {
-            // All The User can see
-            return 'WHERE ('
-                .'(subject.is_private = false OR subject.user_id = '.$user_id.') AND '
-                .'(object.is_private = false OR object.user_id = '.$user_id.') '
-                .') ';
-        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_ADMIN) {
-            return '';
-        }
-    }
-    public function _getActiveNode($relation_record)
-    {
-        $obj = $this->_getGraphReturns($relation_record);
-        if (!$obj) return false;
-        return $this->_isActiveNode($obj['subject'], $obj['relation']) ? $obj['subject'] : $obj['object'];
-    }
-    public function _getPassiveNode($relation_record)
-    {
-        $obj = $this->_getGraphReturns($relation_record);
-        if (!$obj) return false;
-        return $this->_isActiveNode($obj['subject'], $obj['relation']) ? $obj['object'] : $obj['subject'];
-    }
-    public function _buildNodeArr($node)
-    {
-        $ret = [];
-        $ret['identity'] = $node->identity();
-        $ret['labels'] = $node->labels();
-        $ret['values'] = $node->values();
-        return $ret;
-    }
-    public function _buildRelationshipArr($relationship)
-    {
-        $ret = [];
-        $ret['identity'] = $relationship->identity();
-        $ret['type'] = $relationship->type();
-        $ret['values'] = $relationship->values();
-        return $ret;
-    }
-    public function _getGraphReturns($relation_record)
-    {
-        if (!in_array('subject', $relation_record->keys()) ||
-            !in_array('object', $relation_record->keys()) ||
-            !in_array('relation', $relation_record->keys())) return false;
-        return [
-            'subject' => $relation_record->value('subject'),
-            'object' => $relation_record->value('object'),
-            'relation' => $relation_record->value('relation')
-        ];
-    }
-    public function _isActiveNode($node, $relationship)
-    {
-        return $relationship->startNodeIdentity() == $node->identity();
-    }
-
 }
---
+++ src/Model/Table/Neo4jTable.php
@@ -0,0 +1,165 @@
+<?php
+namespace App\Model\Table;
+
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+use Cake\ORM\TableRegistry;
+use Cake\Network\Http\Client;
+use Cake\Cache\Cache;
+
+use App\Model\Table\QuarkTypesTable;
+
+use Cake\Network\Exception\NotFoundException;
+
+use App\Utils\U;
+use App\Utils\NgramConverter;
+
+use Cake\Log\Log;
+use GraphAware\Neo4j\Client\ClientBuilder;
+
+/**
+ * Neo4j Model
+ */
+class Neo4jTable extends AppTable
+{
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+    }
+
+    /****************************************************************************/
+    /* Edit Data                                                                */
+    /****************************************************************************/
+
+    /****************************************************************************/
+    /* Get Data                                                                 */
+    /****************************************************************************/
+    public static function getOnesGraph($name, $privacy_mode = \App\Controller\AppController::PRIVACY_PUBLIC, $user_id = null)
+    {
+        if (($privacy_mode != \App\Controller\AppController::PRIVACY_PUBLIC) && is_null($user_id)) return false;
+
+        // build cypher query
+        $where = self::wherePrivacy($privacy_mode, $user_id);
+        $query = 'MATCH (subject {name: {name}})-[relation]-(object) '
+               .$where
+               .'RETURN DISTINCT subject, object, relation';
+        $parameter = ['name' => $name];
+
+        // connect to neo4j
+        $client = ClientBuilder::create()
+                ->addConnection('http', 'http://neo4j:neo4jn30Aj@localhost:7474')
+                ->build();
+
+        // run cypher
+        $result = $client->run($query, $parameter);
+        if (!$result->records()) return false;
+
+        // format result array
+        $subject = $result->getRecord()->value('subject');
+        $ret = ['subject' => self::buildNodeArr($subject), 'relations' => []];
+
+        foreach ($result->getRecords() as $key => $record) {
+            $active = self::getActiveNode($record);
+            $passive = self::getPassiveNode($record);
+            $relation = $record->value('relation');
+
+            $ret['relations'][] = [
+                'relation' => self::buildRelationshipArr($relation),
+                'active' => self::buildNodeArr($active),
+                'passive' => self::buildNodeArr($passive),
+            ];
+        }
+        // Log::write('debug', $ret);
+        return $ret;
+    }
+
+    /*******************************************************/
+    /* Save Data                                           */
+    /*******************************************************/
+
+    /*******************************************************/
+    /* where                                               */
+    /*******************************************************/
+    public static function wherePrivacy($privacy_mode, $user_id = 1)
+    {
+        if ($privacy_mode == \App\Controller\AppController::PRIVACY_PUBLIC) {
+            // Only Public
+            return 'WHERE subject.is_private = false AND object.is_private = false ';
+        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_PRIVATE) {
+            // Only Private
+            return 'WHERE subject.is_private = true AND subject.user_id = '.$user_id.
+                   ' AND object.is_private = true AND object.user_id = '.$user_id. ' ';
+        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_ALL) {
+            // All The User can see
+            return 'WHERE ('
+                .'(subject.is_private = false OR subject.user_id = '.$user_id.') AND '
+                .'(object.is_private = false OR object.user_id = '.$user_id.') '
+                .') ';
+        } elseif ($privacy_mode == \App\Controller\AppController::PRIVACY_ADMIN) {
+            return '';
+        }
+    }
+
+    /*******************************************************/
+    /* quarks                                              */
+    /*******************************************************/
+    /*******************************************************/
+    /* gluons                                              */
+    /*******************************************************/
+
+    /****************************************************************************/
+    /* Tools                                                                    */
+    /****************************************************************************/
+    public static function buildNodeArr($node)
+    {
+        return [
+            'identity' => $node->identity(),
+            'labels' => $node->labels(),
+            'values' => $node->values()
+        ];
+    }
+    public static function buildRelationshipArr($relationship)
+    {
+        return [
+            'identity' => $relationship->identity(),
+            'type' => $relationship->type(),
+            'values' => $relationship->values()
+        ];
+    }
+    public static function getActiveNode($relation_record)
+    {
+        $obj = self::getGraphReturns($relation_record);
+        if (!$obj) return false;
+        return self::isActiveNode($obj['subject'], $obj['relation']) ? $obj['subject'] : $obj['object'];
+    }
+    public static function getPassiveNode($relation_record)
+    {
+        $obj = self::getGraphReturns($relation_record);
+        if (!$obj) return false;
+        return self::isActiveNode($obj['subject'], $obj['relation']) ? $obj['object'] : $obj['subject'];
+    }
+    public static function getGraphReturns($relation_record)
+    {
+        if (!in_array('subject', $relation_record->keys()) ||
+            !in_array('object', $relation_record->keys()) ||
+            !in_array('relation', $relation_record->keys())) return false;
+        return [
+            'subject' => $relation_record->value('subject'),
+            'object' => $relation_record->value('object'),
+            'relation' => $relation_record->value('relation')
+        ];
+    }
+    public static function isActiveNode($node, $relationship)
+    {
+        return $relationship->startNodeIdentity() == $node->identity();
+    }
+}
